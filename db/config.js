ea248ee41ac588cd0e55d4fd8757f4aea3a92234
Livio Fran Mascarenhas <liviofranm@gmail.com>
2022-11-05 12:15:20 +0530

    server config

--- db/config.js
+++ db/config.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
+const un = process.env.USER_NAME;
 const connection = mysql.createConnection({
   host: process.env.HOST_NAME,
-  user: process.env.USER_NAME,
+  user: "root",
   password: process.env.PASSWORD,
   database: process.env.DB_NAME,
 });
 
 connection.connect((error) => {
+  console.log(process.env.USER_NAME);
   if (error) console.log("error: " + error);
   else console.log("DB connected");
 });
