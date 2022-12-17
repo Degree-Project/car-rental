f4e019d2de49ff0144f6bb6fa0671ef5fd2d4dac
Livio Fran Mascarenhas <liviofranm@gmail.com>
2022-12-17 13:01:36 +0530

    add:get-profile

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,13 +1,14 @@
 const {
   add,
   getUserByEmail,
-  updateUser,
   updateDetails,
+  getProfile,
 } = require("../models/customerModel");
-const { sign } = require("jsonwebtoken");
+const { sign, verify } = require("jsonwebtoken");
+// to check hashed password
 // const { compareSync } = require("bcrypt");
-
 module.exports = {
+  // To signup user
   signup: (req, res) => {
     const body = req.body;
 
@@ -15,22 +16,23 @@ module.exports = {
       if (err) {
         console.log(err);
         return res.status(500).json({
-          success: 0,
+          success: false,
           message: "Database connection error",
         });
       }
       if (!results) {
         return res.json({
-          success: 0,
+          success: false,
           data: "Failed to Register User",
         });
       }
       return res.status(200).json({
-        success: 1,
+        success: true,
         data: results,
       });
     });
   },
+  // To login user
   login: (req, res) => {
     const body = req.body;
     getUserByEmail(body.email, (err, results) => {
@@ -39,35 +41,37 @@ module.exports = {
       }
       if (!results) {
         return res.json({
-          success: 0,
+          success: false,
           data: "Invalid email or password.",
         });
       }
+      // To hash check hashed password
       //   const result = compareSync(body.password, results.password);
       const result = body.password == results.password ? true : false;
       if (result) {
         results.password = undefined;
         const jsonToken = sign({ result: results }, "dbmsproject", {
-          expiresIn: "1h",
+          expiresIn: "2h",
         });
         res.cookie("token", jsonToken, {
           httpOnly: true,
         });
 
         return res.json({
-          success: 1,
+          success: true,
           message: "Login successfully",
           token: jsonToken,
-          user: results
+          user: results,
         });
       } else {
         return res.json({
-          success: 0,
+          success: false,
           data: "Invalid email or password",
         });
       }
     });
   },
+  // To update user details
   updateDetails: (req, res) => {
     const body = req.body;
     updateDetails(body, (err, results) => {
@@ -76,9 +80,46 @@ module.exports = {
         return;
       }
       return res.json({
-        success: 1,
+        success: true,
         message: "Details updated successfully",
       });
     });
+  },
+  // To get user details
+  getProfile: (req, res) => {
+    const token = req.get("cookie").slice(6);
+    console.log(token);
+    if (token === "") {
+      return res.json({
+        success: false,
+        message: "Not Logged In",
+      });
+    }
+    const data = verify(token, "dbmsproject");
+    return res.json({
+      success: true,
+      message: "Profile",
+      data: data.result,
+    });
+  },
+  // To logout user
+  logout: (req, res) => {
+    res.cookie("token", "", {
+      httpOnly: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Logged Out",
+    });
+  },
+  // To check is user is logged in
+  isLoggedIn: (req, res) => {
+    const token = req.get("cookie").slice(6);
+    if (token === "") {
+      return res.json(false);
+    }
+    if (token !== "") {
+      return res.json(true);
+    }
   },
 };
--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -2,6 +2,9 @@ const {
   signup,
   login,
   updateDetails,
+  getProfile,
+  logout,
+  isLoggedIn,
 } = require("../controllers/customerController");
 var router = require("express").Router();
 const { checkToken } = require("../auth/validateToken");
@@ -9,5 +12,8 @@ const { checkToken } = require("../auth/validateToken");
 router.post("/sign-up", signup);
 router.post("/login", login);
 router.post("/update-details", checkToken, updateDetails);
+router.post("/profile", getProfile);
+router.get("/logout", checkToken, logout);
+router.get("/login-status", isLoggedIn);
 
 module.exports = router;
